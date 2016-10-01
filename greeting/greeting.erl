0440dd710fa7d0b1ac9420d1309fe708bde38998
odinsplasmarifle <generalaetius.jbvb@gmail.com>
2016-10-01 21:47:55 +0200

    Initial commit of tut examples

---
+++ greeting/greeting.erl
@@ -0,0 +1,10 @@
+-module(greeting).
+-export([greet/2]).
+
+greet(male, Name) ->
+    io:format("Hello, Mr. ~s!", [Name]);
+greet(female, Name) ->
+    io:format("Hello, Mrs. ~s!", [Name]);
+greet(_, Name) ->
+    io:format("Hello, ~s!", [Name]).
+
